b35e4d24d90c46e7a055b5746184b2003b9e3012
Gustavo Gimenez <gustavogimenez@byronsolutions.com>
2021-08-23 00:52:48 -0300

    Finalizando página de regras e adicionando animações em CSS

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+/*let navBtn = $('regras-menu');
+
+let licSection = $('#licenca');
+let corSection = $('#corrida');
+let campSection = $('#camps');
+let pontSection = $('#pontos');
+let pneSection = $('#pneus');
+let ngrSection = $('#nregras');
+
+let scrollTo = '';
+
+$(navBtn).click(function() {
+
+  let btnId = $(this).attr('id');
+
+  if(btnId == 'navlic') {
+    scrollTo = licSection;
+  } else if(btnId == 'navcor') {
+    scrollTo = corSection;
+  } else if(btnId == 'navcamp') {
+    scrollTo = campSection;
+  } else if(btnId == 'navpont') {
+    scrollTo = pontSection;
+  } else if(btnId == 'navpne') {
+    scrollTo = pneSection;
+  } else if(btnId == 'navnov') {
+    scrollTo = ngrSection;
+  }
+
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $(scrollTo).offset().top - 70
+  }, 1500);
+});*/
+
+});
